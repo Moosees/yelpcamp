4631260743ad8ccf1308847c479d91bb022324d8
Moosees <linus.almgren@gmail.com>
2019-02-23 22:56:16 +0100

    Add new campground form

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
+// DUMMY DATA
+let campgrounds = [
+	{
+		name: 'Crazy Creek',
+		image:
+			'https://images.pexels.com/photos/1549122/pexels-photo-1549122.jpeg?auto=compress&cs=tinysrgb&dpr=1&&w=400'
+	},
+	{
+		name: 'Weird Valley',
+		image: 'https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400'
+	},
+	{
+		name: 'Atlantis Alley',
+		image: 'https://images.pexels.com/photos/772207/pexels-photo-772207.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400'
+	}
+];
+
+// ROUTES
 app.get('/', (req, res) => {
 	res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-	let campgrounds = [
-    { name: 'Crazy Creek', image: 'https://images.pexels.com/photos/1549122/pexels-photo-1549122.jpeg?auto=compress&cs=tinysrgb&dpr=1&&w=400' },
-    { name: 'Weird Valley', image: 'https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400' },
-    { name: 'Atlantis Alley', image: 'https://images.pexels.com/photos/772207/pexels-photo-772207.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=400' }
-  ];
-  
-  res.render('campgrounds', {campgrounds:campgrounds});
+	res.render('campgrounds', { campgrounds: campgrounds });
 });
 
+app.post('/campgrounds', (req, res) => {
+  let newCampground = {name: req.body.name, image: req.body.image};
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+	res.render('new.ejs');
+});
+
+// SERVER SETTINGS
 app.listen(port, () => console.log(`Express is running Yelpcamp app on port ${port}`));
